728946f09dc4683ae6168625a9f58b14d4896c4d
Elaine Zhou <ezhou@stanford.edu>
2014-02-13 05:30:56 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,17 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log('/project/' + idNumber);
+	var proj = $.get('/project/'+idNumber, insertDetails);
+
+}
+
+function insertDetails(result){
+	var id = result['id'];
+	var projectDiv = $('#project'+id);
+	var projectHTML = '<p><small>' + result['date'] + '</small></p>' + '<img src="' + result['image'] + '" class="detailsImage" />' +
+		 result['summary']; 
+	projectDiv.find('.details').append(projectHTML);
 }
 
 /*
@@ -35,4 +46,14 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	var colors = $.get('/palette', updateColors);
+}
+
+function updateColors(palette){
+	var colors = palette['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
